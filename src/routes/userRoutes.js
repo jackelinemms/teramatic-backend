47bec5b49c62ac6237ecbb45d8a9593c275eb3ad
jackelinemms <jackeline.martinst@gmail.com>
2023-09-12 22:48:14 -0300

    completando o arquivo authController.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,6 +6,6 @@ import controllers from "../controllers/userController.js";
 router.get("/", controllers.getAll); //rota pro metodo de find
 router.post("/", controllers.createUser); // rota pro metodo de criar usuario
 router.patch("/:id", controllers.updateUser); //rota para método de atualizar o usuário
-router.patch("/:id", controllers.deleteUser); //rota pra método de deletar o usuário
+router.delete("/:id", controllers.deleteUser); //rota pra método de deletar o usuário
 
 export default router;
